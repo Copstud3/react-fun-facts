7c90cd986c1008b3f9acf632349a0afcb0b7fa1b
Chris <victorchris73@gmail.com>
2024-07-03 15:10:08 +0100

    ...

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function Footer(){
     return(
         <footer>
-            <a href="http://github.com/copstud3" className="footer-text"><span>Made by: <img src="/src/assets/github-svgrepo-com(white).svg" alt="github-logo" className="footer-img" /> @copstud3</span></a>
+            <a href="http://github.com/copstud3" className="footer-text"><span>Made by: <img src="src/assets/github-svgrepo-com(white).svg" alt="github-logo" className="footer-img" /> @copstud3</span></a>
         </footer>
     )
 }
