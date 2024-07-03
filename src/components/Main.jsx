7be9f76c4507fdf58c3ba9356ee6b950cec6c7ad
Chris <victorchris73@gmail.com>
2024-07-03 14:19:03 +0100

    Initial commit

---
+++ src/components/Main.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default function Main(){
+    return(
+        <main>
+            <h1 className="main-text">Fun facts about React</h1>
+            <ul className="main-list">
+                <li>Developed by Jordan Walke at Facebook in 2011.</li>
+                <li>Uses a virtual DOM for efficient updates and rendering.</li>
+                <li>Utilizes JSX, a syntax extension combining JavaScript and HTML.</li>
+                <li>Utilizes JSX, a syntax extension combining JavaScript and HTML.</li>
+                <li>React's principles can be applied to mobile app development with React Native.</li>
+            </ul>
+        </main>
+    )
+}
---
+++ src/components/Nav.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Nav() {
+    return(
+        <nav className="nav">
+            <img src="./src/assets/react.svg" alt="" srcset="" className="nav--img"/>
+            <h1 className="nav--text">ReactFacts</h1>
+        </nav>
+    )
+}
