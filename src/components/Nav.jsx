8d1b11aaae0d616facad8906f0789f639f8717c8
Chris <victorchris73@gmail.com>
2024-07-03 15:28:32 +0100

    ...

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function Nav() {
     return(
         <nav className="nav">
-            <img src="/src/assets/react.svg" alt="" srcset="" className="nav--img"/>
+            <img src="/assets/react.svg" alt="" srcset="" className="nav--img"/>
             <h1 className="nav--text">ReactFacts</h1>
         </nav>
     )
